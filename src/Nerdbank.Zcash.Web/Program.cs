c81568dd151052cd6b4b7f10a6f747b30b9863cc
Andrew Arnott <andrewarnott@live.com>
2023-04-26 12:50:22 -0600

    Add Blazor app to demo what Nerdbank.Zcash can do

---
+++ src/Nerdbank.Zcash.Web/Program.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Nerdbank.Zcash.Web;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
