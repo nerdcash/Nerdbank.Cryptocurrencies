25b2a862904813c9f5fa52ff89616a3ff980ffb4
Andrew Arnott <andrewarnott@live.com>
2023-05-07 08:36:13 -0600

    Fix straggling doc warnings

--- src/Nerdbank.Cryptocurrencies/Bip32HDWallet.PrivateKey.cs
+++ src/Nerdbank.Cryptocurrencies/Bip32HDWallet.PrivateKey.cs
@@ -5,18 +5,32 @@ namespace Nerdbank.Cryptocurrencies;
 
 public static partial class Bip32HDWallet
 {
+	/// <summary>
+	/// An EC private key.
+	/// </summary>
 	internal class PrivateKey : IDisposable
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PrivateKey"/> class.
+		/// </summary>
+		/// <param name="key">The underlying cryptographic key.</param>
 		internal PrivateKey(NBitcoin.Secp256k1.ECPrivKey key)
 		{
 			this.Key = key;
 		}
 
+		/// <summary>
+		/// Gets the underlying cryptographic key.
+		/// </summary>
 		internal NBitcoin.Secp256k1.ECPrivKey Key { get; }
 
 		/// <inheritdoc/>
 		public void Dispose() => this.Key.Dispose();
 
+		/// <summary>
+		/// Creates a public key counterpart to this private key.
+		/// </summary>
+		/// <returns>The public key.</returns>
 		internal PublicKey CreatePublicKey() => new(this.Key.CreatePubKey());
 	}
 }
--- src/Nerdbank.Cryptocurrencies/Bip32HDWallet.PublicKey.cs
+++ src/Nerdbank.Cryptocurrencies/Bip32HDWallet.PublicKey.cs
@@ -5,13 +5,23 @@ namespace Nerdbank.Cryptocurrencies;
 
 public static partial class Bip32HDWallet
 {
+	/// <summary>
+	/// A EC public key.
+	/// </summary>
 	internal class PublicKey
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PublicKey" /> class.
+		/// </summary>
+		/// <param name="key">The underlying cryptographic key.</param>
 		internal PublicKey(NBitcoin.Secp256k1.ECPubKey key)
 		{
 			this.Key = key;
 		}
 
+		/// <summary>
+		/// Gets the underlying cryptographic key.
+		/// </summary>
 		internal NBitcoin.Secp256k1.ECPubKey Key { get; }
 	}
 }
