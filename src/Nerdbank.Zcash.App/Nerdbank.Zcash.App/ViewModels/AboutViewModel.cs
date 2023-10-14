64463b37bd97bdcea2904fed81ed4369bae4ea09
Andrew Arnott <andrewarnott@live.com>
2023-10-14 13:46:32 -0600

    Add disclaimer to About page

--- src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/ViewModels/AboutViewModel.cs
+++ src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/ViewModels/AboutViewModel.cs
@@ -15,6 +15,12 @@ public class AboutViewModel : ViewModelBase
 
 	public string Message => "This app is a Zcash wallet. It seeks to the most intuitive wallet available, while being reliable, secure, and champion some of the best privacy features Zcash has to offer.";
 
+	public string LicenseCaption => "License";
+
+	public string License => """
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+		""";
+
 	public string SupportCommandCaption => "Get support";
 
 	public ReactiveCommand<Unit, Unit> SupportCommand { get; }
