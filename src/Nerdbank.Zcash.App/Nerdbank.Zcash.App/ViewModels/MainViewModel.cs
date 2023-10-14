953882a336f7cfc65065ef7b0c71ffc3daa31087
Andrew Arnott <andrewarnott@live.com>
2023-10-14 07:58:40 -0600

    Add Back button support for android

--- src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/ViewModels/MainViewModel.cs
+++ src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/ViewModels/MainViewModel.cs
@@ -1,25 +1,72 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Windows.Input;
+
 namespace Nerdbank.Zcash.App.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-	private ViewModelBase? content;
+	private readonly Stack<ViewModelBase> viewStack = new();
 
 	public MainViewModel()
 	{
+		this.NavigateBackCommand = ReactiveCommand.Create(
+			() => this.NavigateBack(),
+			this.WhenAnyValue(x => x.Content, new Func<ViewModelBase?, bool>(x => this.viewStack.Count > 1)));
+
 		this.NavigateTo(new HomeScreenViewModel());
 	}
 
 	public ViewModelBase? Content
 	{
-		get => this.content;
-		private set => this.RaiseAndSetIfChanged(ref this.content, value);
+		get => this.viewStack.TryPeek(out ViewModelBase? current) ? current : null;
 	}
 
+	/// <summary>
+	/// Gets the command that navigates back one step in the view stack.
+	/// </summary>
+	public ICommand NavigateBackCommand { get; }
+
+	/// <summary>
+	/// Replaces the entire view stack with a new view model.
+	/// </summary>
+	/// <param name="viewModel">The new view model to select.</param>
+	/// <remarks>
+	/// This is useful primarily at the start of the app, when the user may not see the main home screen right away due to a first launch experience.
+	/// </remarks>
+	public void ReplaceViewStack(ViewModelBase viewModel)
+	{
+		this.viewStack.Clear();
+		this.NavigateTo(viewModel);
+	}
+
+	/// <summary>
+	/// Pushes a view model onto the view stack.
+	/// </summary>
+	/// <param name="viewModel">The new view model.</param>
+	/// <remarks>
+	/// This will no-op if the given view model is already the current view model.
+	/// </remarks>
 	public void NavigateTo(ViewModelBase viewModel)
 	{
-		this.Content = viewModel;
+		if (this.Content != viewModel)
+		{
+			this.viewStack.Push(viewModel);
+			this.RaisePropertyChanged(nameof(this.Content));
+		}
+	}
+
+	/// <summary>
+	/// Pops the current view model off the view stack, effectively moving the view "back" one step.
+	/// </summary>
+	/// <param name="ifCurrentViewModel">The view model that is expected to be on top at the time of the call. If specified, the stack will only be popped if this is the top view model.</param>
+	public void NavigateBack(ViewModelBase? ifCurrentViewModel = null)
+	{
+		if (this.viewStack.Count > 1 && (ifCurrentViewModel is null || this.viewStack.Peek() == ifCurrentViewModel))
+		{
+			this.viewStack.Pop();
+			this.RaisePropertyChanged(nameof(this.Content));
+		}
 	}
 }
--- src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/Views/MainView.axaml.cs
+++ src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/Views/MainView.axaml.cs
@@ -2,10 +2,13 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.ReactiveUI;
+using Nerdbank.Zcash.App.ViewModels;
 
 namespace Nerdbank.Zcash.App.Views;
 
-public partial class MainView : UserControl
+public partial class MainView : ReactiveUserControl<MainViewModel>
 {
 	/// <summary>
 	/// Initializes a new instance of the <see cref="MainView"/> class.
@@ -13,5 +16,34 @@ public partial class MainView : UserControl
 	public MainView()
 	{
 		this.InitializeComponent();
+	}
+
+	protected override void OnLoaded(RoutedEventArgs e)
+	{
+		base.OnLoaded(e);
+
+		if (TopLevel.GetTopLevel(this) is { } topLevel)
+		{
+			topLevel.BackRequested += this.MainView_BackRequested;
+		}
+	}
+
+	protected override void OnUnloaded(RoutedEventArgs e)
+	{
+		if (TopLevel.GetTopLevel(this) is { } topLevel)
+		{
+			topLevel.BackRequested -= this.MainView_BackRequested;
+		}
+
+		base.OnUnloaded(e);
+	}
+
+	private void MainView_BackRequested(object? sender, RoutedEventArgs e)
+	{
+		if (this.ViewModel is { } mainViewModel && mainViewModel.NavigateBackCommand.CanExecute(null))
+		{
+			mainViewModel.NavigateBackCommand.Execute(null);
+			e.Handled = true;
+		}
 	}
 }
