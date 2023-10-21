57f5cec337be5c911c8659d82730283301824699
Andrew Arnott <andrewarnott@live.com>
2023-10-21 08:04:02 -0600

    Workaround for transparent window on win-arm64
    See https://github.com/AvaloniaUI/Avalonia/issues/10405 for more info.

--- src/Nerdbank.Zcash.App/Nerdbank.Zcash.App.Desktop/Program.cs
+++ src/Nerdbank.Zcash.App/Nerdbank.Zcash.App.Desktop/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.ReactiveUI;
 
@@ -17,9 +18,23 @@ internal class Program
 
 	// Avalonia configuration, don't remove; also used by visual designer.
 	public static AppBuilder BuildAvaloniaApp()
-		=> AppBuilder.Configure<App>()
-			.UsePlatformDetect()
-			.WithInterFont()
-			.LogToTrace()
-			.UseReactiveUI();
+	{
+		AppBuilder builder = AppBuilder.Configure<App>()
+				.UsePlatformDetect()
+				.WithInterFont()
+				.LogToTrace()
+				.UseReactiveUI();
+
+		// Workaround for transparent Window on win-arm64 (https://github.com/AvaloniaUI/Avalonia/issues/10405)
+		if (OperatingSystem.IsWindows() && RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
+		{
+			builder = builder.UseWin32()
+				.With(new Win32PlatformOptions
+				{
+					RenderingMode = new[] { Win32RenderingMode.Software },
+				});
+		}
+
+		return builder;
+	}
 }
