06b31bd02b422c74de90171d61af1ade6db219e9
Andrew Arnott <andrewarnott@live.com>
2023-11-14 05:47:55 -0700

    Fix `Account` bug where it's assumed dirty after deserialization
    Bug introduced in abb685fe757d1d592c9d1788c6267f9c36f2b4bf

--- src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/Models/IPersistableData.cs
+++ src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/Models/IPersistableData.cs
@@ -2,10 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.ComponentModel;
+using MessagePack;
 
 namespace Nerdbank.Zcash.App.Models;
 
-public interface IPersistableData : INotifyPropertyChanged
+public interface IPersistableData : INotifyPropertyChanged, IMessagePackSerializationCallbackReceiver
 {
 	bool IsDirty { get; set; }
+
+	void IMessagePackSerializationCallbackReceiver.OnAfterDeserialize()
+	{
+		this.IsDirty = false;
+	}
+
+	void IMessagePackSerializationCallbackReceiver.OnBeforeSerialize()
+	{
+	}
 }
