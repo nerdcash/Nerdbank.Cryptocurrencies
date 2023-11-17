ad4a8bdaea6e37a15c303a920c4fac9f740b51a6
Andrew Arnott <andrewarnott@live.com>
2023-11-16 20:31:41 -0700

    Moved `Bytes20` fixed array into C# 12 inline array

--- src/SharedSource/FixedLengthStructs.cs
+++ src/SharedSource/FixedLengthStructs.cs
@@ -11,23 +11,6 @@ namespace Nerdbank.Zcash.FixedLengthStructs;
 #pragma warning disable SA1649 // File name should match first type name
 #pragma warning disable SA1600 // Elements should be documented
 
-internal unsafe struct Bytes20
-{
-	private const int Length = 20;
-	private fixed byte value[Length];
-
-	internal Bytes20(ReadOnlySpan<byte> value)
-	{
-		value.CopyToWithLengthCheck(this.ValueWritable);
-	}
-
-	[UnscopedRef]
-	internal readonly ReadOnlySpan<byte> Value => MemoryMarshal.CreateReadOnlySpan(ref Unsafe.AsRef(in this.value[0]), Length);
-
-	[UnscopedRef]
-	internal Span<byte> ValueWritable => MemoryMarshal.CreateSpan(ref this.value[0], Length);
-}
-
 internal unsafe struct Bytes32
 {
 	private const int Length = 32;
