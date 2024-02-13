ccde6b34b11f7c82d918fe05437400780221671f
Andrew Arnott <andrewarnott@live.com>
2024-02-13 07:44:25 -0700

    Fix `generate_cs_bindings` script to take a switch

--- src/nerdbank-zcash-rust/generate_cs_bindings.ps1
+++ src/nerdbank-zcash-rust/generate_cs_bindings.ps1
@@ -3,10 +3,14 @@
 <#
 .SYNOPSIS
     Generate C# bindings for the exported rust functions. If uniffi-bindgen-cs is not installed, it will be installed automatically.
+.PARAMETER InstallPrerequisites
+    Install uniffi-bindgen-cs. Default is true if it is not already installed.
 #>
 
-# Check if uniiffi-bindgen-cs is installed
-[bool]$InstallPrerequisites = $null -eq (Get-Command uniffi-bindgen-cs -ErrorAction SilentlyContinue)
+[CmdletBinding()]
+Param (
+    [switch]$InstallPrerequisites = $null -eq (Get-Command uniffi-bindgen-cs -ErrorAction SilentlyContinue)
+)
 
 if ($InstallPrerequisites) {
     cargo install uniffi-bindgen-cs --git https://github.com/NordSecurity/uniffi-bindgen-cs --tag v0.7.0+v0.25.0
