5de1fdbc0de978e2460894a649aa047cf8b912fa
Andrew Arnott <andrewarnott@live.com>
2024-02-28 20:44:10 -0700

    Don't set dirty flag when non-persisted properties change

--- src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/Models/IPersistableData.cs
+++ src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/Models/IPersistableData.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.ComponentModel;
+using System.Reflection;
 using MessagePack;
 
 namespace Nerdbank.Zcash.App.Models;
@@ -9,6 +10,9 @@ namespace Nerdbank.Zcash.App.Models;
 public interface IPersistableData : INotifyPropertyChanged, IMessagePackSerializationCallbackReceiver
 {
 	bool IsDirty { get; set; }
+
+	bool IsPersisted(string? propertyName) => propertyName is not null and not nameof(this.IsDirty)
+		&& this.GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)?.GetCustomAttribute<IgnoreMemberAttribute>() is null;
 
 	void IMessagePackSerializationCallbackReceiver.OnAfterDeserialize()
 	{
