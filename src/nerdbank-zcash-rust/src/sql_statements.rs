2f5e12f48da6665e991759ea4d80270c860d3393
Andrew Arnott <andrewarnott@live.com>
2024-03-17 19:40:11 -0600

    Update sql queries to include orchard

--- src/nerdbank-zcash-rust/src/sql_statements.rs
+++ src/nerdbank-zcash-rust/src/sql_statements.rs
@@ -10,17 +10,18 @@ pub(crate) const GET_TRANSACTIONS_SQL: &str = r#"
 		t.fee_paid,
 		t.block_time,
 		t.expired_unmined,
-		o.output_pool,
-		o.from_account_id,
-		o.to_account_id,
-		o.to_address,
-		s.diversifier,
-		o.value,
-		o.memo
+		txo.output_pool,
+		txo.from_account_id,
+		txo.to_account_id,
+		txo.to_address,
+		coalesce(s.diversifier, o.diversifier) AS diversifier,
+		txo.value,
+		txo.memo
 	FROM v_transactions t
-	LEFT OUTER JOIN v_tx_outputs o ON t.txid = o.txid
+	LEFT OUTER JOIN v_tx_outputs txo ON t.txid = txo.txid
 	LEFT OUTER JOIN transactions tx ON tx.txid = t.txid
 	LEFT OUTER JOIN sapling_received_notes s ON tx.id_tx = s.tx
+	LEFT OUTER JOIN orchard_received_notes o ON tx.id_tx = o.tx
 	WHERE (:account_id IS NULL OR t.account_id = :account_id) AND (t.mined_height IS NULL OR :starting_block IS NULL OR t.mined_height >= :starting_block)
 	ORDER BY t.mined_height, t.tx_index
 "#;
@@ -32,9 +33,10 @@ pub(crate) const GET_BIRTHDAY_HEIGHTS: &str = r#"
 		(SELECT birthday_height FROM accounts WHERE id = :account_id) AS "Original birthday height",
 		(SELECT MIN(mined_height) FROM v_transactions WHERE account_id = :account_id) AS "Block with first note",
 		(SELECT MIN(t.block)
-			FROM sapling_received_notes s
-			INNER JOIN transactions t ON s.tx = t.id_tx
-			WHERE s.account_id = :account_id AND s.spent IS NULL
+			FROM transactions t 
+			LEFT OUTER JOIN sapling_received_notes s ON s.tx = t.id_tx
+			LEFT OUTER JOIN orchard_received_notes o ON o.tx = t.id_tx
+			WHERE (s.account_id = :account_id AND s.spent IS NULL) OR (o.account_id = :account_id AND o.spent IS NULL)
 		) AS "Block with first unspent note"
 "#;
 
@@ -42,13 +44,14 @@ pub(crate) const GET_BIRTHDAY_HEIGHTS: &str = r#"
 pub(crate) const GET_UNSPENT_NOTES: &str = r#"
 	SELECT
 		tx.block,
-		o.value,
-		o.output_pool,
-		COALESCE(o.from_account_id = o.to_account_id, 0) AS is_change
-	FROM v_tx_outputs o
-	INNER JOIN transactions tx ON tx.txid = o.txid
+		txo.value,
+		txo.output_pool,
+		COALESCE(txo.from_account_id = txo.to_account_id, 0) AS is_change
+	FROM v_tx_outputs txo
+	INNER JOIN transactions tx ON tx.txid = txo.txid
 	LEFT OUTER JOIN sapling_received_notes s ON s.tx = tx.id_tx
-	WHERE o.to_account_id = :account_id AND s.spent IS NULL AND o.output_pool > 0
+	LEFT OUTER JOIN orchard_received_notes o ON o.tx = tx.id_tx
+	WHERE txo.to_account_id = :account_id AND s.spent IS NULL AND o.spent IS NULL AND txo.output_pool > 0
 
 	UNION
 	
