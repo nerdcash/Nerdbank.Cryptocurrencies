be24317bddb55a0aec4dc3ee2ae8d02d1d2af285
Andrew Arnott <andrewarnott@live.com>
2024-04-27 12:11:18 -0600

    Ship the desktop app as self-contained on Windows too
    Previous to this, we built a self-contained app only for mac and linux. This left Windows as the only platform that required elevated privileges to install (in the common case that the user doesn't have .NET 8 installed already). In user testing, the UAC prompt on Windows came up for every user and confused every user.

--- azure-pipelines/artifacts/apps.ps1
+++ azure-pipelines/artifacts/apps.ps1
@@ -5,11 +5,15 @@ if (!$BuildConfiguration) {
 }
 
 $VelopackPackageDir = "$RepoRoot/bin/Packages/$BuildConfiguration/velopack"
-$VelopackDataDir = "$RepoRoot/obj/src/Nerdbank.Zcash.App/Nerdbank.Zcash.App.Desktop/x64/$BuildConfiguration/net8.0"
-
-if (!(Test-Path $VelopackPackageDir))  { return }
+if (!(Test-Path $VelopackPackageDir)) { return }
+$VelopackBasePath = "$RepoRoot/obj/src/Nerdbank.Zcash.App/Nerdbank.Zcash.App.Desktop/x64/$BuildConfiguration/net8.0"
+if (!(Test-Path $VelopackBasePath)) { return }
+$VelopackFullPaths = (Get-ChildItem -Recurse "$VelopackBasePath/velopack-data.json")
+if (!$VelopackFullPaths)  { return }
+$VelopackFullPath = $VelopackFullPaths[0]
+$VelopackDataDir = Split-Path $VelopackFullPath
 
 @{
     "$VelopackPackageDir" = (Get-ChildItem $VelopackPackageDir -Recurse);
-    "$VelopackDataDir" = (Get-ChildItem "$VelopackDataDir\velopack-data.json");
+    "$VelopackDataDir" = $VelopackFullPath;
 }
