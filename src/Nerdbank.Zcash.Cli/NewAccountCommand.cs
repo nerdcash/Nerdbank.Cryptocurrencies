06566ddf7cb0572e24a365eb8a9a7de08a363672
Andrew Arnott <andrewarnott@live.com>
2024-06-01 10:22:43 -0600

    Use lowercase verbs for all CLI commands

--- src/Nerdbank.Zcash.Cli/NewAccountCommand.cs
+++ src/Nerdbank.Zcash.Cli/NewAccountCommand.cs
@@ -66,7 +66,7 @@ internal class NewAccountCommand
 
 		Option<ulong> birthdayHeightOption = new("--birthday-height", Strings.BirthdayHeightOptionDescription);
 
-		Command command = new("NewAccount", Strings.NewAccountCommandDescription)
+		Command command = new("new", Strings.NewAccountCommandDescription)
 		{
 			seedPhraseWordLengthOption,
 			seedPhraseOption,
