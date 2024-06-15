nerdcash/Nerdbank.Cryptocurrencies

commit ee117eac869522795e99db607ff3e3ff7d8a53b5

Fix nasty error regarding birthday height
When adding a subsequent account for an existing seed phrase, editing the birthday height tends to temporarily display a nasty error message. This fixes that.

src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/ViewModels/CreateNewAccountDetailsViewModel.cs
@@ -20,7 +20,7 @@ public class CreateNewAccountDetailsViewModel : ViewModelBase
 	private uint index;
 	private string name = string.Empty;
 	private ZcashNetwork network = ZcashNetwork.MainNet;
-	private ulong birthdayHeight = 0;
+	private ulong? birthdayHeight = 0;
 	private ulong? maximumBirthdayHeight;
 
 	[Obsolete("Design-time only", error: true)]
@@ -46,12 +46,7 @@ public class CreateNewAccountDetailsViewModel : ViewModelBase
 			this.CreateBirthdayHeightRecommendation)
 			.ToProperty(this, nameof(this.MinimumBirthdayHeightForHDWalletAdvisory), initialValue: this.CreateBirthdayHeightRecommendation(this.HDWallet, this.Network));
 
-		// TODO: update this to disable the command if *any* validation errors exist -- not just our own hard-coded ones.
-		IObservable<bool> canCreateAccount = this.WhenAnyValue(
-			vm => vm.HasErrors,
-			hasErrors => !hasErrors);
-
-		this.CreateAccountCommand = ReactiveCommand.Create(this.CreateAccount, canCreateAccount);
+		this.CreateAccountCommand = ReactiveCommand.Create(this.CreateAccount, this.IsValid);
 		this.SetBirthdayHeightToTipCommand = ReactiveCommand.CreateFromTask(this.SetBirthdayHeightToTipAsync);
 
 		this.LinkProperty(nameof(this.Network), nameof(this.MinimumBirthdayHeight));
@@ -66,6 +61,7 @@ public class CreateNewAccountDetailsViewModel : ViewModelBase
 
 	public string HDWalletCaption => CreateNewAccountDetailsStrings.HDWalletCaption;
 
+	[Required]
 	public HDWallet? HDWallet
 	{
 		get => this.hdwallet;
@@ -165,7 +161,8 @@ public class CreateNewAccountDetailsViewModel : ViewModelBase
 
 	public string BirthdayHeightCaption => CreateNewAccountDetailsStrings.BirthdayHeightCaption;
 
-	public ulong BirthdayHeight
+	[Required]
+	public ulong? BirthdayHeight
 	{
 		get => this.birthdayHeight;
 		set => this.RaiseAndSetIfChanged(ref this.birthdayHeight, value);
@@ -200,7 +197,8 @@ public class CreateNewAccountDetailsViewModel : ViewModelBase
 
 	public Account CreateAccount()
 	{
-		Verify.Operation(this.HDWallet is not null, "Select an HDWallet first.");
+		Verify.Operation(!this.HasAnyErrors, "Fix validation errors first.");
+		Assumes.NotNull(this.HDWallet);
 
 		Account account = new Account(new ZcashAccount(this.HDWallet.GetZip32HDWalletByNetwork(this.Network), this.Index))
 		{
@@ -228,7 +226,7 @@ public class CreateNewAccountDetailsViewModel : ViewModelBase
 
 	private async ValueTask UpdateBirthdayHeightAsync()
 	{
-		ulong originalBirthdayHeight = this.BirthdayHeight;
+		ulong? originalBirthdayHeight = this.BirthdayHeight;
 
 		ulong currentHeight = await this.LazyHeight.GetValueAsync();
 

commit 42b7eee12991b43d0839f38483346276d9c1730e

Fix secondary account creation
This fixes a regression introduced in ee117eac869522795e99db607ff3e3ff7d8a53b5.

src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/ViewModels/CreateNewAccountDetailsViewModel.cs
@@ -36,7 +36,7 @@ public class CreateNewAccountDetailsViewModel : ViewModelBase
 		this.mainNetChainLength = new(async () => await AppUtilities.GetChainLengthAsync(viewModelServices, ZcashNetwork.MainNet, CancellationToken.None));
 		this.testNetChainLength = new(async () => await AppUtilities.GetChainLengthAsync(viewModelServices, ZcashNetwork.TestNet, CancellationToken.None));
 
-		this.hdwallet = viewModelServices.Wallet.HDWallets.FirstOrDefault();
+		this.HDWallet = viewModelServices.Wallet.HDWallets.FirstOrDefault();
 		this.isHDWalletSelectionVisible = viewModelServices.Wallet.HDWallets.AsObservableChangeSet()
 			.Select(_ => viewModelServices.Wallet.HDWallets.Count > 1)
 			.ToProperty(this, nameof(this.IsHDWalletSelectionVisible));