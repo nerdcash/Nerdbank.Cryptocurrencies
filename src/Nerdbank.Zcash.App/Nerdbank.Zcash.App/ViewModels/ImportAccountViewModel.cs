nerdcash/Nerdbank.Cryptocurrencies

commit 98d0d2734573db5be0803d7fb68aa8e240986aca

Raise default/minimum birthday height for account restore
Due to https://github.com/zcash/librustzcash/issues/1467, restoring an account from sapling activation height (+0 or +1) fails. But since the underlying bug is related to the fact that no sapling commitment tree had started until +2, we can safely just raise the minimum restore height by +2 so users can restore their accounts with the default minimum height

src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/ViewModels/ImportAccountViewModel.cs
@@ -35,7 +35,7 @@ public class ImportAccountViewModel : ViewModelBase, IHasTitle, INotifyDataError
 		this.LinkProperty(nameof(this.SeedPassword), nameof(this.SeedPasswordHasWhitespace));
 		this.LinkProperty(nameof(this.IsTestNet), nameof(this.MinimumBirthdayHeight));
 
-		this.BirthdayHeight = this.NetworkParameters.SaplingActivationHeight;
+		this.BirthdayHeight = this.MinimumBirthdayHeight;
 
 		if (viewModelServices.Wallet.IsEmpty)
 		{
@@ -153,7 +153,7 @@ public class ImportAccountViewModel : ViewModelBase, IHasTitle, INotifyDataError
 
 	public string BirthdayHeightCaption => ImportAccountStrings.BirthdayHeightCaption;
 
-	public ulong MinimumBirthdayHeight => this.NetworkParameters.SaplingActivationHeight;
+	public uint MinimumBirthdayHeight => (uint)(this.NetworkParameters.SaplingActivationHeight + (this.IsTestNet ? 0 : 2)); // workaround https://github.com/zcash/librustzcash/issues/1467
 
 	public string ImportCommandCaption => ImportAccountStrings.ImportCommandCaption;
 

commit f8fd8ba6fe3d6259dc3f92b01bb47d60015f9403

Update default birthday height when switching networks
This regressed in 98d0d2734573db5be0803d7fb68aa8e240986aca

src/Nerdbank.Zcash.App/Nerdbank.Zcash.App/ViewModels/ImportAccountViewModel.cs
@@ -129,13 +129,13 @@ public class ImportAccountViewModel : ViewModelBase, IHasTitle, INotifyDataError
 		get => this.isTestNet;
 		set
 		{
-			bool updateBirthdayHeight = this.BirthdayHeight == this.NetworkParameters.SaplingActivationHeight;
+			bool updateBirthdayHeight = this.BirthdayHeight == this.MinimumBirthdayHeight;
 
 			this.RaiseAndSetIfChanged(ref this.isTestNet, value);
 
 			if (updateBirthdayHeight)
 			{
-				this.BirthdayHeight = this.NetworkParameters.SaplingActivationHeight;
+				this.BirthdayHeight = this.MinimumBirthdayHeight;
 			}
 		}
 	}