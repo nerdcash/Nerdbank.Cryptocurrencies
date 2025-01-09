2d4b72193f610175fcb4bd960c6053ce1cff6449
Andrew Arnott <andrewarnott@live.com>
2025-01-09 08:28:06 -0700

    Fix Licenses job build break when agent only *thinks* `bundle-licenses` is already available

--- src/nerdbank-zcash-rust/Generate-3rdPartyNotices.ps1
+++ src/nerdbank-zcash-rust/Generate-3rdPartyNotices.ps1
@@ -1,6 +1,6 @@
 Push-Location $PSScriptRoot
 
-cargo binstall -y cargo-bundle-licenses --version 2.0.0 --locked
+cargo binstall -y cargo-bundle-licenses --version 2.0.0 --locked --force
 
 New-Item -Type Directory -Path $PSScriptRoot/../../obj/src/nerdbank-zcash-rust | Out-Null
 cargo tree -f "{p} {l}" -e normal | Out-File -Encoding UTF8 -FilePath "$PSScriptRoot/../../obj/src/nerdbank-zcash-rust/THIRD_PARTY_DEPENDENCIES.txt"
